20e22c8740536c2b8ad0466c8efbf1ca17e15eaa
luisaGonzales <luisagc711@gmail.com>
2017-09-26 16:00:40 -0500

    Modificaciones

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,146 +1,155 @@
-'use strict'
-var _asientos = [];
-var pasajeros = [];
+'use strict';
 
-var celdas = document.getElementsByTagName('td');
-for (var i = 0; i < celdas.length; i++) {
-    celdas[i].addEventListener('click',redirect,false);
-    _asientos.push(celdas[i].textContent);
-  }
-function redirect(event){
-    document.getElementById("mostrar").innerHTML= (event.target.textContent);
-    var buscarAsiento = document.getElementById("mostrar").textContent;
-      for(var i = 0; i < pasajeros.length; i++){
-        if(pasajeros[i].asiento == buscarAsiento){
-          document.getElementById("userNombre").value = pasajeros[i].nombre;
-          document.getElementById("userApellido").value = pasajeros[i].apellido;
-          document.getElementById("userDNI").value = pasajeros[i].dni;
+// Datos 
+
+class Pasajero {
+    constructor(nombre, apellido, dni, asiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.asiento = asiento;
+    }
+}
+
+class Avion {
+    constructor() {
+        this._asientos = [];
+        this.pasajeros = [];
+        this.celdas = document.getElementsByTagName('td');
+        for (let i = 0; i < this.celdas.length; i++) {
+            this.celdas[i].onclick = () => (this.redirect(event));
+            this._asientos.push(this.celdas[i].textContent);
         }
-      }
-      limpiarBuscar();
-}
-
-function Pasajero(nombre,apellido,dni, asiento){
-  this.nombre = nombre;
-  this.apellido = apellido;
-  this.dni = dni;
-  this.asiento = asiento;
-}
-function reservar (){
-  var nombre = document.getElementById("userNombre").value;
-  var apellido = document.getElementById("userApellido").value;
-  var dni = document.getElementById("userDNI").value;
-  var asiento = document.getElementById("mostrar").textContent;
-  var imprimir = document.getElementById("imprimir");
-  var comprobar = buscarAsiento();
-  if(comprobar == true){
-    alert("El asiento ya está ocupado");
-  }
-  else if((nombre == "") && (apellido == "") && (dni == "")){
-    alert("El registro está vacío, intentalo nuevamente");
     }
-  else if ((nombre == "") || (apellido == "") || (dni == "")){
-    alert("Faltan ingresar datos");
+    redirect(event) {
+        document.getElementById("mostrar").innerHTML = (event.target.textContent);
+        let buscarAsiento = document.getElementById("mostrar").textContent;
+        console.log(this.pasajeros);
+        for (let i = 0; i < this.pasajeros.length; i++) {
+            if (this.pasajeros[i].asiento == buscarAsiento) {
+                document.getElementById("userNombre").value = this.pasajeros[i].nombre;
+                document.getElementById("userApellido").value = this.pasajeros[i].apellido;
+                document.getElementById("userDNI").value = this.pasajeros[i].dni;
+            }
+        }
+        this.limpiarBuscar();
     }
-  else if(asiento == ""){
-    alert("Falta seleccionar el número de asiento a ser ocupado");
-  }
-  else {
-      pasajeros.push(new Pasajero(nombre, apellido,dni,asiento));
-      imprimir.innerHTML = "<strong>Asiento Registrado</strong> </br>Nombre: " + nombre + "</br>Apellido: " + apellido + "</br>DNI: " + dni + "</br>Asiento:" + asiento;
-      limpiar();
+    reservar() {
+        let nombre = document.getElementById("userNombre").value;
+        let apellido = document.getElementById("userApellido").value;
+        let dni = document.getElementById("userDNI").value;
+        let asiento = document.getElementById("mostrar").textContent;
+        let imprimir = document.getElementById("imprimir");
+        let comprobar = this.buscarAsiento();
+        if (comprobar == true) {
+            alert("El asiento ya está ocupado");
+        } else if ((nombre == "") && (apellido == "") && (dni == "")) {
+            alert("El registro está vacío, intentalo nuevamente");
+        } else if ((nombre == "") || (apellido == "") || (dni == "")) {
+            alert("Faltan ingresar datos");
+        } else if (asiento == "") {
+            alert("Falta seleccionar el número de asiento a ser ocupado");
+        } else {
+            this.pasajeros.push(new Pasajero(nombre, apellido, dni, asiento));
+            imprimir.innerHTML = "<strong>Asiento Registrado</strong> </br>Nombre: " + nombre + "</br>Apellido: " + apellido + "</br>DNI: " + dni + "</br>Asiento:" + asiento;
+            this.limpiar();
+        }
+        let x = this._asientos.indexOf(asiento);
+        this.celdas[x].style.backgroundColor = "#73C6B6";
     }
-  var x = _asientos.indexOf(asiento);
-  celdas[x].style.backgroundColor = "#73C6B6";
-}
-
-function cancelar (){
-  var asiento = document.getElementById("mostrar").textContent;
-  var res;
-  var n = _asientos.indexOf(asiento);
-  celdas[n].style.backgroundColor = "white";
-  for(var i = 0; i < pasajeros.length; i++){
-    if(asiento == pasajeros[i].asiento){
-      pasajeros.splice(i,1);
-      alert("Asiento liberado");
-
-      limpiar();
-
+    cancelar() {
+        let asiento = document.getElementById("mostrar").textContent;
+        let res;
+        let n = this._asientos.indexOf(asiento);
+        this.celdas[n].style.backgroundColor = "white";
+        for (let i = 0; i < this.pasajeros.length; i++) {
+            if (asiento == this.pasajeros[i].asiento) {
+                this.pasajeros.splice(i, 1);
+                alert("Asiento liberado");
+                this.limpiar();
+            }
+        }
     }
-  }
-
-    console.log(pasajeros);
-}
-
-
-function listar (){
-  var listado = document.getElementById("listado");
-  var lista = "<h4>Listado de asientos ocupados</h4>";
-  if(datos == []){
-    alert("Todos los asientos están vacíos")
-  } else {
-    for (var i = 0; i < pasajeros.length; i++){
-      var datos = pasajeros[i];
-      var sNombre = datos.nombre ;
-      var sApellido = datos.apellido;
-      var sDni = datos.dni;
-      var sAsiento = datos.asiento;
-      lista += "<div><strong> Asiento: " + sAsiento + "</strong> <br/>Nombre:" + sNombre + "</br>Apellido: " + sApellido + "</br> DNI: " + sDni + "</div> </br>";
-      listado.innerHTML = lista;
+    listar() {
+        let listado = document.getElementById("listado");
+        let lista = "<h4>Listado de asientos ocupados</h4>";
+        if (datos == []) {
+            alert("Todos los asientos están vacíos")
+        } else {
+            for (let i = 0; i < this.pasajeros.length; i++) {
+                let datos = this.pasajeros[i];
+                let sNombre = datos.nombre;
+                let sApellido = datos.apellido;
+                let sDni = datos.dni;
+                let sAsiento = datos.asiento;
+                lista += "<div><strong> Asiento: " + sAsiento + "</strong> <br/>Nombre:" + sNombre + "</br>Apellido: " + sApellido + "</br> DNI: " + sDni + "</div> </br>";
+                listado.innerHTML = lista;
+            }
+        }
     }
-  }
-}
-
-function buscarPorDni (){
-  var dniBuscado = document.getElementById("buscarDNI").value;
-  var dniEncontrado = document.getElementById("dniEncontrado");
-  var res = "";
-  for(var i = 0; i < pasajeros.length; i++){
-    if(dniBuscado == pasajeros[i].dni){
-      res = "<strong>DNI: </strong>" + dniBuscado + "<br/> Nombre: " + pasajeros[i].nombre + "<br/>Apellido: " + pasajeros[i].apellido + "<br/>Asiento: " + pasajeros[i].asiento;
-      break;
-    } else {
-      res = "<strong>DNI: </strong>" + dniBuscado + "<br/> DNI no registrado";
+    buscarPorDni() {
+        let dniBuscado = document.getElementById("buscarDNI").value;
+        let dniEncontrado = document.getElementById("dniEncontrado");
+        let res = "";
+        for (let i = 0; i < this.pasajeros.length; i++) {
+            if (dniBuscado == this.pasajeros[i].dni) {
+                res = "<strong>DNI: </strong>" + dniBuscado + "<br/> Nombre: " + this.pasajeros[i].nombre + "<br/>Apellido: " + this.pasajeros[i].apellido + "<br/>Asiento: " + this.pasajeros[i].asiento;
+                break;
+            } else {
+                res = "<strong>DNI: </strong>" + dniBuscado + "<br/> DNI no registrado";
+            }
+        }
+        dniEncontrado.innerHTML = res;
     }
-  }
-  dniEncontrado.innerHTML = res;
-
-}
-function buscarAsiento (){
-  var asiento = document.getElementById("mostrar").textContent;
-  var res;
-  for(var i = 0; i < pasajeros.length; i++){
-    if(asiento == pasajeros[i].asiento){
-      res = true;
+    buscarAsiento() {
+        let asiento = document.getElementById("mostrar").textContent;
+        let res;
+        for (let i = 0; i < this.pasajeros.length; i++) {
+            if (asiento == this.pasajeros[i].asiento) {
+                res = true;
+            } else {
+                res = false;
+            }
+        }
+        return res;
     }
-    else{
-      res = false;
+    limpiar() {
+        let inputs = document.getElementsByTagName("input");
+        let espacioAsiento = document.getElementById("mostrar");
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = "";
+        }
+        espacioAsiento.innerHTML = "";
     }
-  }
-  return res;
+    limpiarLista() {
+        let listado = document.getElementById("listado");
+        listado.innerHTML = "";
+    }
+    limpiarBuscar() {
+        let espacio = document.getElementById("dniEncontrado");
+        let input = document.getElementById("buscarDNI");
+        espacio.innerHTML = "";
+        input.value = "";
+    }
+    limpiarReservar() {
+        let espacio = document.getElementById("imprimir");
+        espacio.innerHTML = "";
+        this.limpiar();
+    }
+    iniciar() {
+        $("#btnBuscarDNI").click(() => this.buscarPorDni());
+        $("#btnReservar").click(() => this.reservar());
+        $("#btnCancelar").click(() => this.cancelar());
+        $("#btnLimpiarReservar").click(() => this.limpiarReservar());
+        $("#btnListar").click(() => this.listar());
+        $("#btnLimpiarLista").click(()=> this.limpiarLista());
+    }
 }
 
-function limpiar(){
-  var inputs = document.getElementsByTagName("input");
-  var espacioAsiento = document.getElementById("mostrar");
-  for(var i = 0; i < inputs.length; i++){
-    inputs[i].value = "";
-  }
-  espacioAsiento.innerHTML = "";
-}
-function limpiarLista(){
-  var listado = document.getElementById("listado");
-  listado.innerHTML = "";
-}
-function limpiarBuscar(){
-  var espacio = document.getElementById("dniEncontrado");
-  var input = document.getElementById("buscarDNI");
-  espacio.innerHTML = "";
-  input.value = "";
+// let avion = new Avion;
 
-}
-function limpiarReservar(){
-  var espacio = document.getElementById("imprimir");
-  espacio.innerHTML = "";
-  limpiar();
-}
+$(document).ready(() => {
+    
+        let avion = new Avion();
+        avion.iniciar();
+    })
